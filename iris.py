28eb4f49b3b4ed30b293aac32b62ac4daf4f1306
mau5atron <gabrielbetancourt96@outlook.com>
2018-10-16 16:40:24 -0400

    decision region method finished

---
+++ iris.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+df = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data', header=None)
+
+y = df.iloc[0:100].values
+y = np.where(y == 'Iris-setosa', -1, 1)
+X = df.iloc[0:100, [0,2]].values
+
+plt.scatter(X[50:100, 0], X[50:100, 1], color='blue', marker='x', label='versicolor')
+plt.scatter(X[:50, 0], X[:50, 1], color='red', marker='o', label='setosa')
+plt.xlabel('sepal length [cm]')
+plt.ylabel('petal length [cm]')
+plt.legend(loc='upper left')
+plt.show()
